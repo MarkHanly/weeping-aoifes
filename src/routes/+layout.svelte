9915a091481f8d499b34f371f179c037426caf7c
Mark Hanly <m.hanly@unsw.edu.au>
2026-05-23 17:11:08 +1000

    Added meta info and favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
 	import './layout.css';
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
 	import Header from '$lib/components/Header.svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	
+  <meta property="og:title" content="Weeping Aoifes" />
+  <meta property="og:description" content="Explore the Blindboy Podcast" />
+  <meta property="og:image" content="$lib/assets/android-chrome-512x512.png" />
+  <meta property="og:url" content="https://weeping-aoifes.netlify.app/" />
+  <meta property="og:type" content="website" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content="Weeping Aoifes" />
+  <meta name="twitter:description" content="Explore the Blindboy Podcast" />
+  <meta name="twitter:image" content="$lib/assets/android-chrome-512x512.png" />
+
+</svelte:head>
 <Header />
 {@render children()}
 
